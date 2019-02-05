5b48dc193ceba5aae0489a89c38fb606b42145a5
Juan Vivas <juanlvo@gmail.com>
2019-02-05 11:41:15 +0100

    feat: implementation of the full assigment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  password = '****';
+  messages = [];
+  numberOfMessage = 0;
+
+  onClickDetails() {
+    if (this.password == '****') {
+      this.password = 'tuna';
+      this.messages.push(this.numberOfMessage);
+    } else {
+      this.password = '****';
+      this.messages.push(this.numberOfMessage);
+    }
+    this.numberOfMessage++;
+  }
+
+  getColorBackground(message) {
+    if (message >= 5) {
+      return 'blue';
+    } else {
+      return 'white';
+    }
+  }
+
+  getMessageColor(message) {
+    if (message >= 5) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
